393fe5c3755b66714e93257b3af54ca599bd32f6
shaokiat <shaokiat@gmail.com>
2022-08-10 18:48:23 +0800

    fix: add Link tags

--- pages/financial_advisory.js
+++ pages/financial_advisory.js
@@ -1,89 +1,79 @@
 import Head from 'next/head'
 import styles from '../styles/financialservices.module.css'
 
-const agent1 = new URL('../public/Agent1.JPG', import.meta.url)
-const agent2 = new URL('../public/Agent2.jpg', import.meta.url)
-const agent3 = new URL('../public/Agent3.jpg', import.meta.url)
-
 import { MdKeyboardBackspace, MdOutlineBusinessCenter } from 'react-icons/Md'
 import { FaPenSquare } from 'react-icons/Fa'
+import Link from 'next/link'
+
+const data = [
+  {
+    name: 'Jonathan',
+    specialization: 'Specialized in SME businesses',
+    department: 'Financing (Equity / Debt)',
+    image: new URL('../public/Agent1.JPG', import.meta.url),
+  },
+  {
+    name: 'Michelle',
+    specialization: 'Specialized in Personal Finance',
+    department: 'Investment (Stocks / Bonds)',
+    image: new URL('../public/Agent2.JPG', import.meta.url),
+  },
+  {
+    name: 'Su-Ann',
+    specialization: 'Specialized in Retirement Planning',
+    department: 'Budgeting / Investment',
+    image: new URL('../public/Agent3.JPG', import.meta.url),
+  },
+  {
+    name: 'Su-Ann',
+    specialization: 'Specialized in Retirement Planning',
+    department: 'Budgeting / Investment',
+    image: new URL('../public/Agent3.JPG', import.meta.url),
+  },
+  {
+    name: 'Su-Ann',
+    specialization: 'Specialized in Retirement Planning',
+    department: 'Budgeting / Investment',
+    image: new URL('../public/Agent3.JPG', import.meta.url),
+  },
+]
 
 export default function financial_advisory() {
   return (
-    <Head>
+    <div>
       <div className="container">
-        <a href="/financial_services" className={styles.adviseBack}>
-          <MdKeyboardBackspace size={40} />
-        </a>
+        <Link href="/financial_services">
+          <a className={styles.adviseBack}>
+            <MdKeyboardBackspace size={30} />
+          </a>
+        </Link>
       </div>
-      <div className="row">
-        <div className="col-3 offset-2">
-          <a
-            href="https://wa.me/93266791?text=Hi%20there!%20I%27m%20interested%20in%20finding%20out%20more%20about%20your%20financial%20services."
-            target="_blank"
-            className={styles.agentCards}
-            rel="noreferrer"
-          >
-            <div className={styles.imgwrapper}>
-              <img className={styles.Agent1} src={agent1}></img>
+      <div className="d-flex justify-content-center align-content-center">
+        <div className="row justify-content-center d-flex flex-wrap w-75">
+          {data.map((banker) => (
+            <div className="col-4" key={banker._id}>
+              <Link href="#">
+                <a className={styles.agentCards} rel="noreferrer">
+                  <div className={styles.imgwrapper}>
+                    <img className={styles.Agent1} src={banker.image}></img>
+                  </div>
+                  <div className={styles.agentName}>
+                    <h1>{banker.name}</h1>
+                  </div>
+                  <div className={styles.agentDescription}>
+                    <MdOutlineBusinessCenter size={40} />{' '}
+                    {banker.specialization}
+                    <br></br>
+                    <br></br>
+                    <FaPenSquare size={40} />
+                    {banker.department}
+                  </div>
+                </a>
+              </Link>
             </div>
-            <div className={styles.agentName}>
-              <h1> Johnathan </h1>
-            </div>
-            <div className={styles.agentDescription}>
-              <MdOutlineBusinessCenter size={40} /> Specialized in SME
-              businesses
-              <br></br>
-              <br></br>
-              <FaPenSquare size={40} /> Financing (Equity / Debt)
-            </div>
-          </a>
-        </div>
-
-        <div className="col-3">
-          <a
-            href="https://wa.me/93266791?text=Hi%20there!%20I%27m%20interested%20in%20finding%20out%20more%20about%20your%20financial%20services."
-            target="_label"
-            className={styles.agentCards}
-          >
-            <div className={styles.imgwrapper}>
-              <img className={styles.Agent1} src={agent2}></img>
-            </div>
-            <div className={styles.agentName}>
-              <h1> Michelle </h1>
-            </div>
-            <div className={styles.agentDescription}>
-              <MdOutlineBusinessCenter size={40} /> Specialized in Personal
-              Finance
-              <br></br>
-              <br></br>
-              <FaPenSquare size={40} /> Investment (Stocks / Bonds)
-            </div>
-          </a>
-        </div>
-
-        <div className="col-3">
-          <a
-            href="https://wa.me/93266791?text=Hi%20there!%20I%27m%20interested%20in%20finding%20out%20more%20about%20your%20financial%20services."
-            target="_label"
-            className={styles.agentCards}
-          >
-            <div className={styles.imgwrapper}>
-              <img className={styles.Agent1} src={agent3}></img>
-            </div>
-            <div className={styles.agentName}>
-              <h1> Su-Ann </h1>
-            </div>
-            <div className={styles.agentDescription}>
-              <MdOutlineBusinessCenter size={40} /> Specialized in Retirement
-              Planning
-              <br></br>
-              <br></br>
-              <FaPenSquare size={40} /> Budgeting / Investment
-            </div>
-          </a>
+          ))}
         </div>
       </div>
-    </Head>
+    </div>
   )
 }
--- pages/insurance.js
+++ pages/insurance.js
@@ -6,56 +6,71 @@ import { AiFillCar, AiOutlineHome } from 'react-icons/Ai'
 import { BsUmbrella } from 'react-icons/Bs'
 import { RiSeedlingLine } from 'react-icons/Ri'
 import { TbWheelchair } from 'react-icons/Tb'
+import Link from 'next/link'
 
 export default function financial_advisory() {
   return (
     <Head>
       <div className="container">
-        <a href="/client" className={styles.insuranceBack}>
-          <MdKeyboardBackspace size={40} />
-        </a>
+        <Link href="/client">
+          <a className={styles.insuranceBack}>
+            <MdKeyboardBackspace size={40} />
+          </a>
+        </Link>
       </div>
 
       <div className="row offset-2">
         <div className="col">
-          <a href="#" className={styles.logo}>
-            <MdHealthAndSafety size={100} />
-            <h3> Health Insurance</h3>
-          </a>
+          <Link href="#">
+            <a className={styles.logo}>
+              <MdHealthAndSafety size={100} />
+              <h3> Health Insurance</h3>
+            </a>
+          </Link>
         </div>
 
         <div className="col">
-          <a href="#" className={styles.logo}>
-            <AiFillCar size={100} />
-            <h3> Auto Insurance</h3>
-          </a>
+          <Link href="#">
+            <a className={styles.logo}>
+              <AiFillCar size={100} />
+              <h3> Auto Insurance</h3>
+            </a>
+          </Link>
         </div>
         <div className="col">
-          <a href="#" className={styles.logo}>
-            <AiOutlineHome size={100} />
-            <h3> Home Insurance</h3>
-          </a>
+          <Link href="#">
+            <a className={styles.logo}>
+              <AiOutlineHome size={100} />
+              <h3> Home Insurance</h3>
+            </a>
+          </Link>
         </div>
       </div>
 
       <div className="row offset-2">
         <div className="col">
-          <a href="#" className={styles.logo}>
-            <BsUmbrella size={100} />
-            <h3> Umbrella Insurance</h3>
-          </a>
+          <Link href="#">
+            <a className={styles.logo}>
+              <BsUmbrella size={100} />
+              <h3> Umbrella Insurance</h3>
+            </a>
+          </Link>
         </div>
         <div className="col">
-          <a href="#" className={styles.logo}>
-            <RiSeedlingLine size={100} />
-            <h3> Life &nbsp; Insurance</h3>
-          </a>
+          <Link href="#">
+            <a className={styles.logo}>
+              <RiSeedlingLine size={100} />
+              <h3> Life &nbsp; Insurance</h3>
+            </a>
+          </Link>
         </div>
         <div className="col">
-          <a href="#" className={styles.logo}>
-            <TbWheelchair size={100} />
-            <h3> Disability Insurance</h3>
-          </a>
+          <Link href="#">
+            <a className={styles.logo}>
+              <TbWheelchair size={100} />
+              <h3> Disability Insurance</h3>
+            </a>
+          </Link>
         </div>
       </div>
     </Head>
