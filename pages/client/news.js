a4d12c9be22444696e7a22a9eae084977d627e3d
xwang049 <xwang049@e.ntu.edu.sg>
2022-08-10 12:21:53 +0800

    news

--- pages/client/news.js
+++ pages/client/news.js
@@ -1,12 +1,65 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/alt-text */
 
-const News = () => {
-  return (
-    <div className="container">
-      News
-      <div>Hi</div>
-    </div>
-  )
+
+
+import React from 'react'
+import Head from 'next/head'
+import Image  from 'next/image'
+
+
+import { news } from '../data/news'
+import styles from "../../styles/news.module.css";
+
+const imgStyle = {
+  width: '100%',
+  height: '500px',
+  // backgroundImage: 'url(' + bg + ')',
+  backgroundPosition: 'center 0',
+  backgroundSize: '2045px 472px',
+  backgroundRepeat: 'no-repeat',
 }
 
-export default News
+const Home = () => {
+  return (
+    <>
+      <div className="oneCiti navbar-brand ml-20" href="#">
+        OneCiti
+      </div>
+      <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      <div className={styles["container"]}>
+        {
+          news.map(item => <div className={styles.item} key={item.id}>
+            <div className={styles["item-head"]}>
+              <div className={styles.time}>{item.time}</div>
+              <div className={styles.date}>{item.date}</div>
+            </div>
+            
+            <Image src={item.pic} alt="" className={styles['item-img']}  height={200} width={300}/>
+            <div className={styles["item-content"]}>{item.title}</div>
+          </div>)
+        }
+        {/* <div className="row">
+          <Image style={imgStyle.news1} alt="" src={nvidiaNews} />
+        </div> */}
+      </div>
+      <div style={{borderRadius: '500px', overflow: 'hidden'}}>
+        <Image src="/1.jpg" layout="fill" objectFit="cover" />
+      </div>
+    </>
+  )
+}
+export default Home
+
+
+
+
---
+++ pages/data/news.js
@@ -0,0 +1,35 @@
+import p1 from "./1.jpg";
+import p2 from "./2.jpg";
+import p3 from "./3.jpg";
+import p4 from "./4.jpg";
+export const news = [
+    {
+        id:1,
+        title:'Bank of England raises rates by half-point, its biggest hike in over 27 years',
+        pic: p1,
+        date: '2022-08-10',
+        time: "7.30 pM",
+    },
+    {
+        id:2,
+        title:'Corporate Radar: Airtel, GAIL to turn ex-dividend; ITC, Zomato Q1 results; AGMs, and more',
+        pic: p2,
+        date: '2022-08-10',
+        time: "7.30 pM",
+    },
+    {
+        id:1,
+        title:'Indian-American named President of UBS Americas, CEO of UBS Americas holding',
+        pic: p3,
+        date: '2022-08-10',
+        time: "7.30 pM",
+    },
+    {
+        id:1,
+        title:'How to invest during inflationary times? Here are 4 ways for retail investors',
+        pic: p4,
+        date: '2022-08-10',
+        time: "7.30 pM",
+    }
+
+]
