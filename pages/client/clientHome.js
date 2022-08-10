bae1ece70e6b152e1f01634e6e333119cb5c07a8
shaokiat <shaokiat@gmail.com>
2022-08-10 21:50:04 +0800

    fix: clientHome page

--- pages/client/clientHome.js
+++ pages/client/clientHome.js
@@ -1,84 +1,94 @@
-import Head from 'next/head'
 import styles from '../../styles/clienthomepage.module.css'
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
-import { MdAccountCircle } from 'react-icons/Md'
-import {FaBalanceScale, FaFantasyFlightGames} from 'react-icons/Fa'
-import {HiUserGroup,HiOutlineAcademicCap} from 'react-icons/Hi'
-import {BiCalculator} from 'react-icons/Bi'
+import { MdAccountCircle } from 'react-icons/md'
+import { FaBalanceScale, FaFantasyFlightGames } from 'react-icons/fa'
+import { HiUserGroup, HiOutlineAcademicCap } from 'react-icons/hi'
+import { BiCalculator } from 'react-icons/bi'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-<span className = {styles.header}>
-            <h1>OneCiti</h1>
-        </span>
-        <span>
-        <h1 className = {styles.header}> Welcome Back, User</h1>
-        </span>
+      <span className={styles.header}>
+        <h1>OneCiti</h1>
+      </span>
+      <span>
+        <h1 className={styles.header}> Welcome Back, User</h1>
+      </span>
 
-        <div class = "row">
-                    <div class = "col-3 offset-2">
-                        <a className = {styles.categoryLogo} href = "../account_bal">
-                        <MdAccountCircle size = {120} />
-                        </a>
-                        <a className = {styles.categoriesHeaders} href = "../account_bal">
-                          My Accounts
-                        </a>
+      <div className="row justify-content-center">
+        <div className="col-3">
+          <Link href="../account_bal">
+            <a className={styles.categoryLogo}>
+              <MdAccountCircle size={120} />
+            </a>
+          </Link>
+          <Link href="../account_bal">
+            <a className={styles.categoriesHeaders}>My Accounts</a>
+          </Link>
+        </div>
 
-                      </div>
-                      
-                      <div class = "col-3">
-                           <a className = {styles.categoryLogo} href = "https://www.citibank.com.sg/SGGCB/JSO/username/signon/flow.action?ts=1660136063283" target="_blank">
-                               <FaBalanceScale size = {120} />
-                            </a>
-                            <a className = {styles.categoriesHeaders} href = "https://www.citibank.com.sg/SGGCB/JSO/username/signon/flow.action?ts=1660136063283" target="_blank">
-                          Stock Exchange
-                        </a>
-                      </div>
-                      
-                    <div class = "col-3">
-                    <a className = {styles.categoryLogo} href = "../financial_services">
-                               <HiUserGroup size = {120} />
-                            </a>
-                            <a className = {styles.categoriesHeaders} href = "#">
-                          Financial Services
-                        </a>
-                    </div>
-            </div>
-         <div class = "row">
-        
-                      <div class = "col-3 offset-2">
-                      <a className = {styles.btmcategoryLogo} href = "/academy">
-                               <HiOutlineAcademicCap size = {120} />
-                            </a>
-                            <a className = {styles.btmcategoriesHeaders} href = "/academy">
-                          Academy
-                        </a>
-                      </div>
-                      
-                      <div class = "col-3">
-                      <a className = {styles.btmcategoryLogo} href = "#">
-                               <BiCalculator size = {120} />
-                            </a>
-                            <a className = {styles.btmcategoriesHeaders} href = "#">
-                          Financial Tools
-                        </a>
-                        
-                      </div>
-                      <div class = "col-3">
-                      <a className = {styles.btmcategoryLogo} href = "#">
-                               <FaFantasyFlightGames size = {120} />
-                            </a>
-                            <a className = {styles.btmcategoriesHeaders} href = "#">
-                          Financial Game
-                        </a>
-                      </div>
-            </div>
-            </div>
-  );
+        <div className="col-3">
+          <Link href="https://www.citibank.com.sg/SGGCB/JSO/username/signon/flow.action?ts=1660136063283">
+            <a className={styles.categoryLogo} target="_blank" rel="noreferrer">
+              <FaBalanceScale size={120} />
+            </a>
+          </Link>
+          <Link href="https://www.citibank.com.sg/SGGCB/JSO/username/signon/flow.action?ts=1660136063283">
+            <a
+              className={styles.categoriesHeaders}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Stock Exchange
+            </a>
+          </Link>
+        </div>
+
+        <div className="col-3">
+          <Link href="../financial_services">
+            <a className={styles.categoryLogo}>
+              <HiUserGroup size={120} />
+            </a>
+          </Link>
+          <Link href="../financial_services">
+            <a className={styles.categoriesHeaders}>Financial Services</a>
+          </Link>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-3">
+          <Link href="/academy">
+            <a className={styles.btmcategoryLogo}>
+              <HiOutlineAcademicCap size={120} />
+            </a>
+          </Link>
+          <Link href="/academy">
+            <a className={styles.btmcategoriesHeaders}>Academy</a>
+          </Link>
+        </div>
+
+        <div className="col-3">
+          <Link href="#">
+            <a className={styles.btmcategoryLogo}>
+              <BiCalculator size={120} />
+            </a>
+          </Link>
+          <Link href="#">
+            <a className={styles.btmcategoriesHeaders}>Financial Tools</a>
+          </Link>
+        </div>
+        <div className="col-3">
+          <Link href="#">
+            <a className={styles.btmcategoryLogo}>
+              <FaFantasyFlightGames size={120} />
+            </a>
+          </Link>
+          <Link href="#">
+            <a className={styles.btmcategoriesHeaders}>Financial Game</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
