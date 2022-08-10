1f87f6eafaa5ce3c18be6e0cd9cefbcf0ab46596
shaokiat <shaokiat@gmail.com>
2022-08-10 21:56:41 +0800

    fix pages

--- pages/academy.js
+++ pages/academy.js
@@ -1,30 +1,27 @@
 import Head from 'next/head'
 import styles from '../styles/financialservices.module.css'
 
-
-import {MdKeyboardBackspace} from 'react-icons/Md'
+import { MdKeyboardBackspace } from 'react-icons/Md'
 
 export default function financial_services() {
-    return (
-        <Head>
-                <title>Financial Services</title>
-                <div class = "container">
-                    <a href = "/client/clientHome" className = {styles.BackButton}>
-                    <MdKeyboardBackspace size = {40} />
-                    </a>
-                    <div class = "row">
-                        <a className = {styles.Service1} href = "/financial_advisory">
-                            <h1 className = {styles.test}> Financial Advisory</h1>
-                        </a>
-                    </div>
-                    <div class = "row">
-                    <a className = {styles.Service2} href = "/insurance">
-                            <h1>Insurance</h1>
-                        </a>
-                    </div>
-                </div>
-
-        </Head>
-
-    ) 
-    }
+  return (
+    <Head>
+      <title>Financial Services</title>
+      <div className="container">
+        <a href="/client/clientHome" className={styles.BackButton}>
+          <MdKeyboardBackspace size={40} />
+        </a>
+        <div className="row">
+          <a className={styles.Service1} href="/financial_advisory">
+            <h1 className={styles.test}> Financial Advisory</h1>
+          </a>
+        </div>
+        <div className="row">
+          <a className={styles.Service2} href="/insurance">
+            <h1>Insurance</h1>
+          </a>
+        </div>
+      </div>
+    </Head>
+  )
+}
