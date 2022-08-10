6f289b554f831bf43179a18b393af4a5f733eb93
xwang049 <xwang049@e.ntu.edu.sg>
2022-08-10 21:25:48 +0800

    news01

--- pages/client/financialtools.js
+++ pages/client/financialtools.js
@@ -1,15 +1,31 @@
+// import Head from 'next/head'
+// import React from 'react'
+
+
+// const financialtools = () => {
+//   return( 
+//     <>
+//       <div className="oneCiti navbar-brand ml-20" href="#">
+//         OneCiti Mortgage Calculator
+//       </div>
+      
+//     </>
+  
+//   )
+// }
+
+// export default financialtools
+
 import React from 'react'
 import Head from 'next/head'
 import Image  from 'next/image'
-import { Base64 } from 'js-base64'
+
 import { news } from '../data/news'
-
 import styles from "../../styles/news.module.css";
-
 
 const imgStyle = {
   width: '100%',
-  height: '600px',
+  height: '500px',
   //backgroundImage: 'url(' + bg + ')',
   backgroundPosition: 'center 0',
   backgroundSize: '2045px 472px',
@@ -19,10 +35,9 @@ const imgStyle = {
 const Home = () => {
   return (
     <>
-      <head>
-        <title>News</title>
-      </head>
-      
+      <Head>
+        <title>Welcome to OneCiti</title>
+      </Head>
       <div className={styles["container"]}>
         {
           news.map(item => <div className={styles.item} key={item.id}>
@@ -30,14 +45,13 @@ const Home = () => {
               <div className={styles.time}>{item.time}</div>
               <div className={styles.date}>{item.date}</div>
             </div>
-            <Image src={item.pic} alt="" className={styles['item-img']}  height={300} width={600}/>
+            <Image src={item.pic} alt="" className={styles['item-img']}  height={200} width={300}/>
             <div className={styles["item-content"]}>{item.title}</div>
           </div>)
         }
         {/* <div className="row">
           <Image style={imgStyle.news1} alt="" src={nvidiaNews} />
         </div> */}
-        
       </div>
     </>
   )
