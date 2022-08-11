e5d87acb6642a6584b80e0fcca08cb1b6d79aac7
LucasLeow <leowjianhoong@gmail.com>
2022-08-11 16:57:28 +0800

    resized bankerHome

--- pages/banker/index.js
+++ pages/banker/index.js
@@ -15,48 +15,27 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Welcome to OneCiti</title>
+          <title>Welcome to OneCiti</title>
       </Head>
 
       <div>
-        <div className="row">
-          <div className={styles.header}>OneCiti</div>
-        </div>
-
-        <div classNameName="row">
-          <div classNameName="col">
-            <div classNameName="row">
-              <div classNameName="col-4">
-                <a href="#" className={styles.box1}>
-                  <div>
-                    <BsReverseLayoutTextSidebarReverse size={100} />
-                  </div>
-                  <div className={styles.logoHeader1}> Daily Tasks </div>
-                </a>
-              </div>
-
-              <div classNameName="col-4">
-                <a href="#" className={styles.box1}>
-                  <div>
-                    <RiBankFill size={100} />
-                  </div>
-                  <div className={styles.logoHeader1}> Bank Services </div>
-                </a>
+          <div className = "row">
+              <div className = {styles.header}>
+                  OneCiti
               </div>
             </div>
 
-<<<<<<< HEAD
                 <div className = "row">
-                      <div className = "col">
+                      <div className = "col-5">
                             <div className = "row">
-                                  <div className = "col-2">
+                                  <div className = "col-4">
                                               <a href = "#" className = {styles.box1}>
                                                     <div> <BsReverseLayoutTextSidebarReverse size={100} />  </div>
                                                     <div className = {styles.logoHeader1}> Daily Tasks </div>
                                               </a>
                                   </div>
 
-                                  <div className = "col-2">
+                                  <div className = "col-3">
                                       <a href = "#" className = {styles.box1}>
                                             <div> <RiBankFill size={100} /> </div>
                                             <div className = {styles.logoHeader1}> Bank Services </div>
@@ -66,13 +45,13 @@ export default function Home() {
                             </div>
 
                             <div className = "row">
-                                <div className = "col-2">
+                                <div className = "col-4">
                                       <a href = "#" className = {styles.box1}>
                                             <div> <HiUserGroup size={100} /> </div>
                                             <div className = {styles.logoHeader1}> Clients</div>
                                         </a>
                                 </div>
-                                <div className = "col-2">
+                                <div className = "col-3">
                                         <a href = "#" className = {styles.box1}>
                                               <div> <TiNews size={100} /> </div>
                                               <div className = {styles.logoHeader1}> Knowledge Management </div>
@@ -130,76 +109,12 @@ export default function Home() {
                           </div>
                       </div>
                 </div>
-=======
-            <div classNameName="row">
-              <div classNameName="col-4">
-                <a href="#" className={styles.box1}>
-                  <div>
-                    <HiUserGroup size={100} />
-                  </div>
-                  <div className={styles.logoHeader1}> Clients</div>
-                </a>
-              </div>
-              <div classNameName="col-4">
-                <a href="#" className={styles.box1}>
-                  <div>
-                    <TiNews size={100} />
-                  </div>
-                  <div className={styles.logoHeader1}>Knowledge Management</div>
-                </a>
-              </div>
->>>>>>> b7df4c736e8810ddf03831f7f3561156244769a5
             </div>
-          </div>
 
-          <div classNameName="col">
-            <div className={styles.glancebg}>
-              <div classNameName="row">
-                <div className={styles.newsHeader}>At A Glance</div>
-              </div>
-              <div classNameName="row">
-                <a href="#" className={styles.article1}>
-                  <BsReverseLayoutTextSidebarReverse size={50} /> Daily Tasks
-                  <div className={styles.articleBody1}>
-                    <br></br>
-                    <li>Approve accounts for Jenny, George and Cynthia</li>
-                    <li>Due : Today </li>
-                  </div>
-                </a>
 
-                <a href="#" className={styles.article2}>
-                  <HiUserGroup size={50} /> Clients
-                  <div className={styles.articleBody1}>
-                    <br></br>
-                    <li>Client Meeting with Jessica</li>
-                    <li>Due : Today 14:00</li>
-                  </div>
-                </a>
 
-                <a href="#" className={styles.article3}>
-                  <TiNews size={50} /> Daily Tasks
-                  <div className={styles.articleBody1}>
-                    <br></br>
-                    <li>Check and complete accounts for Donuts.co</li>
-                    <li>Due : 14 August 2022</li>
-                  </div>
-                </a>
 
-                <a href="#" className={styles.article4}>
-                  <TiNews size={50} /> Knowledge Management
-                  <div className={styles.articleBody1}>
-                    <br></br>
-                    <li>
-                      New City Promotion : 5% cashback for new cardholders
-                    </li>
-                    <li>Due : 31 August 2022</li>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+
     </>
   )
 }
